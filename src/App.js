5eb47684400d96f7614a784a1071ce471dad8164
priyajevale <priyajevale0625@gmail.com>
2024-01-24 00:29:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+// import logo from './logo.svg';
+// import "./App.css";
+// import ExpenseItem from './components/ExpenseItem'
+import React from 'react';
+import Expenses from './components/Expenses/Expenses';
+
+const App =() => {
+  const expenses = [
+    {
+      id:'e1' ,title: 'toilet Paper', amount: 94.12, date: new Date(2021, 6, 28) ,
+    },
+    { 
+      id:'e2', title: 'new tv', amount: 29.32, date: new Date(2022, 5, 3),
+    },
+    { 
+      id:'e3' ,title: 'car insurance', amount: 67.12, date: new Date(2019, 2, 28),
+    },
+    {
+       id:'e4', title: 'New Desk', amount: 56.5, date: new Date(2023, 2, 28), 
+      },
+  ];
+  // return React.createElement('div', {},React.createElement('h2',{},"lets get started"),React.createElement(Expenses,{items:expenses}) 
+  // );
+  return (
+    <div>
+      <h2>lets get started</h2>
+      <Expenses items={expenses}/>
+      </div>
+      
+  );
+}
+
+export default App;
